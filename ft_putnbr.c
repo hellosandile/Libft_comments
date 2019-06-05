13ee970756c2e8ae3a28ca5518a2f4562ef92df6
Sandile MKHIZE <samkhize@c5r1s1.wethinkcode.co.za>
2019-06-05 16:47:28 +0200

    5 June work

---
+++ ft_putnbr.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samkhize <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 15:50:18 by samkhize          #+#    #+#             */
+/*   Updated: 2019/06/05 16:46:41 by samkhize         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* This function will display the integer n on the standard output. This
+ * function is similar to out ft_itoa function because we will be doing it
+ * recursively. We will be breaking down int number as a whole into individual
+ * single digit numbers and we will   */
