ef2db37e92625f1a61e0864b52a215d5db9b2e37
Sandile Mkhize <mkhizenathan@gmail.com>
2019-06-07 12:57:52 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,6 +17,12 @@ for strncpy(3) and strncat(3). THe strlcat() function appends the NUL-terminated
 string src to the end of dst. It will append at most size - strlen(dst) - 1 bytes,
 NUL-terminating the results
 
+strlcat() appends string src to the end of dst.  It will append at most
+dstsize - strlen(dst) - 1 characters.  It will then NUL-terminate, unless
+dstsize is 0 or the original dst string was longer than dstsize (in prac-
+tice this should not happen as it means that either dstsize is incorrect
+or that dst is not a proper string).
+
 RETURN VALUES: The strlcpy() and strlcat() functions return the total length of
 the string they tried to create. For strlcpy() that means the length of src.
 For strlcat() that means the initial length of dst plus the length of src.*/
