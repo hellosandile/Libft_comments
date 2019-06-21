698d7f52d2d1e9aadbe96645333ff4f867feaf0f
Sandile Mkhize <mkhizenathan@gmail.com>
2019-06-21 12:50:21 +0200

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,4 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
+/*This is my function with the NULL error on Libfttest*/
+{
+  size_t  i;
+  size_t  j;
+  
+  i = 0;
+  if (haystack[i] == '\0')
+    return (NULL);
+  if (!needle[0] || needle == haystack)
+    return ((char *)haystack);
+  j = 0;
+  while (i + j < len && haystack[i] != '\0')
+  {
+    j = 0;
+    while (haystack[i + j] == needle[j])
+      j++;
+    if (needle[j] == '\0')
+      return ((char *)haystack + i);
+    i++;
+  }
+  return (NULL);
+}
+/*Submitted function*/
+{
+  size_t  i;
+  size_t  j;
+  
+  i = 0;
+  while (i < len && haystack[i] != '\0')
+  {
+    j = 0;
+    while (i +j < len && haystack[i + j] == needle[j] && needle[j] != '\0')
+      j++;
+    if (needle[j] == '\0')
+      return ((char *)haystack + i);
+    i++;
+  }
+  return (NULL);
+}
